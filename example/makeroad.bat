2f7753f73530812a96306d762ea2aa407319419d
Jacob Kolding <dacobi@gmail.com>
2023-03-06 11:53:17 +0100

    makeroad.bat

--- example/makeroad.bat
+++ example/makeroad.bat
@@ -2,8 +2,8 @@ del VERAROAD.PRG
 cl65 -O -t cx16 veraroad.c  -o VERAROAD.PRG
 
 copy roadmap\map.bin MAP1.BIN
-copy roadtmap\tiles.bin TILES.BIN
-copy roadtmap\pal.bin PAL.BIN
+copy roadmap\tiles.bin TILES.BIN
+copy roadmap\pal.bin PAL.BIN
 copy roadmap\map0.bin MAP0.BIN
 
 x16emu  -prg VERAROAD.PRG -run
