4fda6e0671e7885092f833e314958eff84d84877
Jacob Kolding <dacobi@gmail.com>
2023-03-06 11:51:48 +0100

    example update

---
+++ example/makevera4pp.bat
@@ -0,0 +1,8 @@
+del VERA4.PRG
+cl65 -O -t cx16 vera4bpp.c  -o VERA4.PRG
+
+copy tmappot\map.bin MAP.BIN
+copy tmappot\tiles.bin TILES.BIN
+copy tmappot\pal.bin PAL.BIN
+
+x16emu  -prg VERA4.PRG -run
