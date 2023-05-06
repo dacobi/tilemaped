97fc42978a876cdcdfc297cb1f1f2973944634eb
Jacob Kolding <dacobi@gmail.com>
2023-05-06 15:01:26 +0200

    version 1.2.6

--- version.h
+++ version.h
@@ -1,4 +1,5 @@
-std::string TilemapEd_Version = "1.2.4"; // 332
+std::string TilemapEd_Version = "1.2.6"; // 334
+//std::string TilemapEd_Version = "1.2.4"; // 332
 //std::string TilemapEd_Version = "1.2.0"; // 328
 //std::string TilemapEd_Version = "1.0.9"; // 315
 //std::string TilemapEd_Version = "1.0.4"; // 310
