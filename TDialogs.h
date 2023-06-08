1caa27c644b548159f1fcc9b5eea27f7841b51cb
Jacob Kolding <dacobi@gmail.com>
2023-06-08 17:55:56 +0200

    DialogValueFile::cancel

--- TDialogs.h
+++ TDialogs.h
@@ -100,7 +100,7 @@ class DialogElement{
 		DTDialog *mParent;
 		bool bSameLine = false;
 		int mCondition = -1;
-		virtual void render(){if(mCondition > -1){if(mParent->mCondition != mCondition){return;}}  if(bSameLine){ImGui::SameLine();}}
+		virtual void render(){if(mCondition > -1){if(mParent->mCondition != mCondition){return;}} if(bSameLine){ImGui::SameLine();}}
 };
 
 class DialogElementText : public DialogElement{
@@ -257,7 +257,7 @@ class DialogValueFile : public DialogValueType<std::string>{
 		
 		DialogValueFile(DTDialog *cParent, int cCond, std::string cLabel, std::string cFileExt, std::string cFileKey, std::string cDefault, std::string* cTarget, bool cMustExist, bool cMustBeFile, bool cMustBeFolder, bool cMustNotBeFile, bool cMustNotExist, bool cMustBeProject){mParent = cParent; mDefault = cDefault; mValue = mDefault; mTarget = cTarget; mCondition = cCond; mTextInput.bMustExist = cMustExist; mTextInput.bMustBeFile = cMustBeFile; mTextInput.bMustBeFolder = cMustBeFolder; mTextInput.bMustNotBeFile = cMustNotBeFile; mTextInput.bMustNotExist = cMustNotExist; mTextInput.bMustBeProject = cMustBeProject; mLabel = cLabel; mFileKey = "ChooseFileDlgKey" + cFileKey; mFileExt = cFileExt; mTextInput.bIsInputActive = true; mTextInput.bAutoComplete = true;}		
 		virtual void render();
-		virtual void cancel(){if(mCondition > -1){if(mParent->mCondition != mCondition){return;}} mValue = mDefault; mTextInput.mDialogTextMain = mDefault; bIsValid = false;}
+		virtual void cancel(){mValue = mDefault; mTextInput.mDialogTextMain = mDefault; bIsValid = false;}
 };
 
 
