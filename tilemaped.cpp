00c7ae9c839de8b178c7a526f256e5317815ad6d
Jacob Kolding <dacobi@gmail.com>
2023-06-10 09:44:13 +0200

    HelpText Update

--- tilemaped.cpp
+++ tilemaped.cpp
@@ -531,16 +531,16 @@ int TSettings::runOCD(int mode){
 
 void TSettings::initHelpText(){
 
-	mHelpText.push_back("WINDOWS: TilemapEd has 5 main windows which are TileMap Editor, Tile Editor\nPalette Editor, TileSet Editor and Selection Editor.\nSelect Window in View menu and press (SPACE) to quickly switch back to the last active window.\nPress (F2) to view the Project Info dialog. When working with Tile BPPs\nlower than 8 the Palette Offset dialog can be shown/hidden in the View menu.");	
+	mHelpText.push_back("WINDOWS: TilemapEd has 6 main windows which are TileMap Editor, Tile Editor\nPalette Editor, TileSet Editor, Selection Editor and Sprite Editor.\nSelect Window in View menu and press (SPACE) to quickly switch back to the last active window.\nPress (F2) to view the Project Info dialog. When working with Tile BPPs\nlower than 8 the Palette Offset dialog can be shown/hidden in the View menu.");	
 	mHelpText.push_back("UNDO: Most changes to TileMap(s) and Tiles can be undone. Press (U) to undo\nand (R) to redo action. Some operations will clear the Undo Stack.");
 	mHelpText.push_back("SELECTION: Tiles and Pixels can be selected using the mouse. Hold (LEFT SHIFT)\nand (LEFT MOUSE BUTTON) to select a range by dragging.\nHold (LEFT SHIFT) and press (RIGHT MOUSE BUTTON)\nto modify Selection on a Tile/Pixel basis.\nPress (A) to select All, (N) to select None and (I) to Invert selection.");
 	mHelpText.push_back("BRUSHES: Press (F8) to open the Brush List, then select <width> and <height>\nand Press <Add Brush>. Press (RIGHT MOUSE BUTTON) in TileSet or Palette to add Brush Elements.\nPress (F9) to activate (next) Brush and (F10) to drop selected Brush.");
 	mHelpText.push_back("INPUT: When entering a value using a Slider Widget hold (LEFT CONTROL) and press\n(LEFT MOUSE BUTTON) on the widget to turn it into a TextInput Widget.");
+	mHelpText.push_back("ZOOM/SCROLL: In Editor windows, where an object (ie. TileMap/Tile(s)/Sprite Frame)\nis being edited, the object can be Zoomed In/Out using (MOUSE WHEEL) and Scrolled\nUp/Down/Left/Right by holding (LEFT CONTROL) and grabbing with (LEFT MOUSE BUTTON).");
 	mHelpText.push_back("INI FILE: When using the CLI arguments:\n\"--opengl, --d3d, --software, --window, --maximize, --highdpi <uiscaleinpercent>,\n--nohighdpi, --vsync, --novsync\"\nthe chosen options will be saved to \""+mINIPath+"\" and used until\nother options are applied. Options can also be changed in \"File->Settings\".");
 
 	mHelpTextMap.push_back("PLACE TILE: To place a Tile in the TileMap press (LEFT MOUSE BUTTON) to select\na Tile in the TileSet and then (LEFT MOUSE BUTTON) to place it in the TileMap.");
-	mHelpTextMap.push_back("SELECT TILE: Press (RIGHT MOUSE BUTTON) to select a Tile in the TileMap.\nPress (S) to show all Tiles of the Selected Tile Type.\nPress (RIGHT MOUSE BUTTON) on a Tile in the TileSet\nto replace all selected Tiles in the TileMap.");
-	mHelpTextMap.push_back("SCROLL/SCALE: Use (MOUSE SCROLL WHEEL) to scale the TileMap and Scroll the TileSet.\nHold (LEFT CONTROL) and (LEFT MOUSE BUTTON) to move the TileMap.");	
+	mHelpTextMap.push_back("SELECT TILE: Press (RIGHT MOUSE BUTTON) to select a Tile in the TileMap.\nPress (S) to show all Tiles of the Selected Tile Type.\nPress (RIGHT MOUSE BUTTON) on a Tile in the TileSet\nto replace all selected Tiles in the TileMap.");	
 	mHelpTextMap.push_back("TILESET: Press (F3) to create an empty Tile. Press (F4) to create a copy of the selected Tile.\nPress (F5) to Rotate Tile Left. Press (F6) to Rotate Tile Right.\nPress (DEL) to Remove the Selected Tile if it's not used in any TileMap.\nOr Select \"Remove Unused Tiles\" in the Edit menu to remove all Tiles not used in any TileMap.\nTiles can be reordered using drag and drop or using the <Move Up>/<Move Down> buttons.\nHolding (LEFT CONTROL) while dragging will replace the Target Tile with a copy of the Source.");
 	mHelpTextMap.push_back("FLIP TILE: Press (F) to cycle through flip values. Press (X) to flip the selected Tile\nHorizontally and (Y) to flip Vertically.");
 	mHelpTextMap.push_back("PALETTE OFFSET: Tiles from the TileSet will have the current Palette Offset.");
@@ -550,14 +550,14 @@ void TSettings::initHelpText(){
 	mHelpTextTile.push_back("SELECT PIXEL: Press (RIGHT MOUSE BUTTON) to select a color in the Tile.\nPress (S) to show all Pixels of the Selected color.\nPress (RIGHT MOUSE BUTTON) on a color in the Palette\nto replace all selected Pixels in the Tile.");	
 	mHelpTextTile.push_back("GRID: Press (P) to show Pixel Grid in Tile Editor.");
 
-	mHelpTextTileSet.push_back("EDITOR: The TileSet Editor works much like the Tile Editor, only on the entire TileSet.\nYou can set the Tile Grid Width and Tile Scale in the Edit menu under \"TileSet Edit\"\nor Use (MOUSE WHEEL). Use (LEFT CONTROL) and (LEFT MOUSE BUTTON) to Scroll the TileSet.");
+	mHelpTextTileSet.push_back("EDITOR: The TileSet Editor works much like the Tile Editor, only on the entire TileSet.\nYou can set the Tile Grid Width and Tile Scale in the Edit menu under \"TileSet Edit\".");
 	mHelpTextTileSet.push_back("GRID: Press (P) to show Pixel Grid in TileSet Editor and Press (T) to show the Tile Grid.");
 
-	mHelpTextSelection.push_back("EDITOR: The Selection Editor works much like the TileSet Editor, only without Pixel selection.\nTo enter Selection Editor you must make a Rectangular Selection in a TilaMap of min 2x2\nand max 16x16 Tiles. Then choose Selection in the View menu.\nIf the Selection is not a Rectangle of min 2x2 and max 16x16 an Error Message will be shown.\nYou can set the Tile Scale in the Edit menu under \"Selection Edit\" or Use (MOUSE WHEEL).\nUse (LEFT CONTROL) and (LEFT MOUSE BUTTON) to Scroll the Selection.");
+	mHelpTextSelection.push_back("EDITOR: The Selection Editor works much like the TileSet Editor, only without Pixel selection.\nTo enter Selection Editor you must make a Rectangular Selection in a TilaMap of min 2x2\nand max 16x16 Tiles. Then choose Selection in the View menu.\nIf the Selection is not a Rectangle of min 2x2 and max 16x16 an Error Message will be shown.\nYou can set the Tile Scale in the Edit menu under \"Selection Edit\".");
 	mHelpTextSelection.push_back("GRID: Press (P) to show Pixel Grid in Selection Editor and Press (T) to show the Tile Grid.");
 
 	mHelpTextPalette.push_back("CHANGE COLOR: Press (LEFT MOUSE BUTTON) to select a Color in the Palette Editor.\nUse RGB sliders or Press (LEFT MOUSE BUTTON) on the Pick Selected Color Box.");
-	mHelpTextPalette.push_back("COPY COLOR: Press (RIGHT MOUSE BUTTON) on a Color to copy its value to the selected Color.");
+	mHelpTextPalette.push_back("COPY COLOR: Press (RIGHT MOUSE BUTTON) on a Color to copy its value to the Selected Color.");
 	mHelpTextPalette.push_back("IMPORT: Select <Import Palette> in the Edit menu to import a palette file.\nGimp palettes and TilemapEd palettes are supported.");
 	mHelpTextPalette.push_back("APPLY/CANCEL: Press <Apply Changes> to update the Project Palette.\nPress <Cancel Changes> to revert the Palette Editor to the Project Palette.");
 
@@ -567,8 +567,9 @@ void TSettings::initHelpText(){
 	mHelpTextImport.push_back("TILE: Tiles can be imported from PNG and RAW, when BPP is 8 or 4.\nPNGs must have BPP 8 and Height/Width equal to TileSizeX/TileSizeY.\nRAW binary files must have filesize of (TileSizeX*TileSizeY) divided by pixels pr byte.\nIf BPP is 4, Tile pixel values from PNG will be \"pixelvalue % 16\"");
 	mHelpTextImport.push_back("TILESET: TileSets can be imported from Project file or PNG.\nProject files must have the same BPP as current project.\nPNGs must have Width and Height where \"Width % TileSizeX\" equals zero.\nIf BPP is 4, Tile pixel values from PNG will be \"pixelvalue % 16\"."); 
 	mHelpTextImport.push_back("TILEMAP: TileMaps can be imported from Project files. A Tile value offset can be specified.\nThe final TileMap must not have any Tile values larger than the current TileSet.\nTo Import an existing TileMap with its TileSet, first Import the TileSet.\nThen open Project Info and take note of the Tile number of the first Tile in the TileSet.\nThen Import the TileMap with Tile offset set to the given Tile number.");
+	mHelpTextImport.push_back("SPRITE: Sprites and Sprite Frames can be imported from Project file, PNG and RAW.\nPNGs must have Width and Height where \"Width % FrameSizeX\" equals zero.\nIf Sprite BPP is 4, Frame pixel values from PNG will be \"pixelvalue % 16\"."); 
 
-	mHelpTextSprite.push_back("EDITOR: The Sprite Editor works much like the Tile or TileSet Editor.\nSprite Frames can be Zoomed/Scrolled using (MOUSE SCROLL WHEEL)/(LEFT CONTROL) + (LEFT MOUSE BUTTON)\nSprite Frames can be Scrolled Through using (LEFT SHIFT) + (MOUSE SCROLL WHEEL)\nSprites have their own Brush Lists. One for BPP 8 and one for BPP 4.");
+	mHelpTextSprite.push_back("EDITOR: The Sprite Editor works much like the Tile or TileSet Editor.\nSprite Frames can be Scrolled Through using (LEFT SHIFT) + (MOUSE WHEEL)\nSprites have their own Brush Lists. One for BPP 8 and one for BPP 4.\nYou can set the Sprite Frame Scale in the Edit menu under \"Sprite Edit\".");
 	mHelpTextSprite.push_back("SPRITE TOOLS: The \"Edit->Sprite Tools\" menu contains various tools for\nCopying/Scaling/Rotating Sprites/Sprite Frames.");
 	mHelpTextSprite.push_back("TEXTURE FILTERING: When using Sprite Scale/Rotate functions Texture Filtering can be\nenabled in \"Edit->Texture Filtering\". The Options are: \"None\", \"Some\", \"All\".");
 }
