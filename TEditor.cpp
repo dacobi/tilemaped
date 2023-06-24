ca84c6460c8ffa546cce58b80f4aa9ef9f50aa25
Jacob Kolding <dacobi@gmail.com>
2023-06-24 10:47:15 +0200

    version 1.6.36

--- TEditor.cpp
+++ TEditor.cpp
@@ -254,7 +254,7 @@ void TEditor::initDialogs(){
 
 	mClipboardMap.init("Maps","Tile", TBRUSH_TILE, &bShowClipboardMap,mGlobalSettings.mGlobalTexParam.TexSizeX, mGlobalSettings.mGlobalTexParam.TexSizeY, &mGlobalSettings.TileMapScale, mGlobalSettings.TileMapScale, &mCurrentBrushTile, &mGlobalSettings.mGlobalTexParam);
 	mClipboardMap.bUseTileOffset = true;
-	mClipboardMap.MinScale = 1;
+	mClipboardMap.MinScale = 4;
 	mClipboardMap.MaxScale = 32;
 
 	mSprite4.TexBPP = 4;
