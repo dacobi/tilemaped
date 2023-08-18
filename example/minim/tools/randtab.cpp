0440011074542a7c82644f6c3f4eee934072de04
Jacob Kolding <dacobi@gmail.com>
2023-08-18 20:46:10 +0200

    randtab

---
+++ example/minim/tools/randtab.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string>
+#include <cmath>
+#include <iostream>
+
+signed char randtab[100];
+
+int main(){
+	
+	srand(0);
+
+	std::cout << "signed char randtab[100] = {";
+	
+	for(int ri = 0; ri < 100; ri++){
+		std::cout << (int)(rand() % 255) - 127;
+		if(ri != 99){
+			std::cout << ",";
+		}
+	}
+	
+	std::cout << "};" << std::endl;
+	
+	return 0;	
+}
